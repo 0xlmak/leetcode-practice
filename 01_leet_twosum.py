05d413d3d19ae2539f3279361c168e21015aa1d4
0xlmak <ludovic.makong@gmail.com>
2023-02-28 18:46:47 +0100

    add revised solution to leetcode #01 easy

--- 01_leet_twosum.py
+++ 01_leet_twosum.py
@@ -1,22 +1,22 @@
+from typing import List
+
 class Solution(object):
-    def twoSum(self, nums, target):
-        
-        id1 = 0 # first tracker to find the first index
-        id2 = 0 # second tracker to find the second index
-        result = []
-        found = 0 # boolean to track when the index pair is found
-       
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        id1: int = 0  # first tracker to find the first index
+        id2: int = 0  # second tracker to find the second index
+        found: bool = False  # boolean to track when the index pair is found
+        result: List[int] = []
+
         # loop over the list as a reference until the second index is found
-        while(id1<len(nums) and found==0):
-            # loop over the list resulting from the absolute difference with the target until the reference is found  
-             while(id2<len(nums) and found==0):
-                 if ((id2 != id1) and (nums[id1] == abs(target - nums[id2]))):
-                     found = 1
-                     result.append(id1)
-                     result.append(id2)
-                 id2 = id2 + 1
-            # if nothing is found go over the next reference and reset the second loop
-             if (id2>=len(nums) and found==0):
-                 id1 = id1 + 1
-                 id2 = 0
+        while id1 < len(nums) and found is False:
+            # loop over the diff with the target until the ref is found
+            while id2 < len(nums) and found is False:
+                if (id2 != id1) and (nums[id1] + nums[id2] == target):
+                    result.append(id1)
+                    result.append(id2)
+                    break
+                id2 = id2 + 1
+            # if nothing is found go over the next ref and reset the loop
+            id1 = id1 + 1
+            id2 = 0
         return result
