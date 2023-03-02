26b6d25f1f5e3f050d110c89715d51bb7f2359e0
0xlmak <ludovic.makong@gmail.com>
2023-03-02 18:50:15 +0100

    add revised solution to leetcode #01 easy

--- 01_leet_twosum.py
+++ 01_leet_twosum.py
@@ -1,22 +1,9 @@
 from typing import List
 
-class Solution(object):
+class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        id1: int = 0  # first tracker to find the first index
-        id2: int = 0  # second tracker to find the second index
-        found: bool = False  # boolean to track when the index pair is found
-        result: List[int] = []
-
-        # loop over the list as a reference until the second index is found
-        while id1 < len(nums) and found is False:
-            # loop over the diff with the target until the ref is found
-            while id2 < len(nums) and found is False:
-                if (id2 != id1) and (nums[id1] + nums[id2] == target):
-                    result.append(id1)
-                    result.append(id2)
-                    break
-                id2 = id2 + 1
-            # if nothing is found go over the next ref and reset the loop
-            id1 = id1 + 1
-            id2 = 0
-        return result
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+        
