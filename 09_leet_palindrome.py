1353f03e9670513d3e3ed0dfbdd3d1a0914e77b3
0xlmak <ludovic.makong@gmail.com>
2023-03-09 09:22:54 +0000

    add typing and formatting revisions to leetcode #01 and #09 solutions

--- 09_leet_palindrome.py
+++ 09_leet_palindrome.py
@@ -1,6 +1,7 @@
 from typing import List
 
-class Solution(object):
+
+class Solution:
     def isPalindrome(self, x: List[int]) -> bool:
         """
         :type x: int
@@ -9,9 +10,10 @@ class Solution(object):
         # convert integer input to string
         x_lst: str = str(x)
         # set the reversed version of the input as a string
-        reverse_x_lst: str = ''
-        for i in range(len(x_lst)-1, -1, -1):
+        reverse_x_lst: str = ""
+        for i in range(len(x_lst) - 1, -1, -1):
             reverse_x_lst += x_lst[i]
-
-        # compare strings of the integer input and its reversed to determine whether it is a palindrome or not
+        
+        # compare strings of the integer input and its reversed 
         return x_lst == reverse_x_lst
+        
