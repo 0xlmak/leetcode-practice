ffebf8538079de09cf26a54f0fc536123a5e2d14
0xlmak <ludovic.makong@gmail.com>
2023-03-12 19:18:49 +0000

    add style revisions on leetcode #14 solution

--- 14_leet_longestcommonprefix.py
+++ 14_leet_longestcommonprefix.py
@@ -6,7 +6,7 @@ class Solution:
         result: str = ""
 
         # set each character of the first string as prefix
-        for i in range(len(strs[0]):
+        for i in range(len(strs[0])):
             # check if the prefix is common with other strings
             for s in strs:
                 # stop when out of bounds or no character is common anymore
