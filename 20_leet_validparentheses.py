3212c33f1f2805ecbcd62fa0ea65a315808b07b0
0xlmak <ludovic.makong@gmail.com>
2023-03-14 15:48:22 +0000

    add style revisions on leetcode #20 solution

--- 20_leet_validparentheses.py
+++ 20_leet_validparentheses.py
@@ -11,11 +11,11 @@ class Solution:
         stack: List[str] = []  # set a stack to store every found open bracket
         for bracket in s:
             if bracket in bracket_pairs:
-                stack.append(bracket)  # stack each open bracket found till a closed one 
+                stack.append(bracket)  # stack each open bracket found till a closed one
                 continue
             if (
                 not stack or bracket != bracket_pairs[stack.pop()]
-           ):  # no open bracket or no valid pair is found
+            ):  # no open bracket or no valid pair is found
                 return False
         # valid pairs were found leading to empty the stack
         return len(stack) == 0
