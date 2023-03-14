2659743b38f1ac32b40caa2b3e436d04541c0b1b
0xlmak <ludovic.makong@gmail.com>
2023-03-14 15:32:56 +0000

    add type revisions on leetcode #20 solution

--- 20_leet_validparentheses.py
+++ 20_leet_validparentheses.py
@@ -1,11 +1,11 @@
 from typing import List
 from typing_extensions import TypedDict
 
-Parentheses = TypedDict("Parentheses", {"(": str, "[": str, "{": str})
+Parentheses = TypedDict("Parentheses", {'(': str, '[': str, '{': str})
 
 class Solution:
     def isValid(self, s: str) -> bool:
-        bracket_pairs: Parentheses = {"(": ")", "[": "]", "{": "}"}  # set bracket pairs symbols 
+        bracket_pairs: Parentheses = {'(': ')', '[': ']', '{': '}'}  # set bracket pairs symbols 
         stack: List[str] = []  # set a stack to store every found open bracket
         for bracket in s:
             if bracket in bracket_pairs:
