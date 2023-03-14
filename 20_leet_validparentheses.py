bb3c72e6f6bd0f608bbca3fb0660a89e6683a382
0xlmak <ludovic.makong@gmail.com>
2023-03-14 15:43:55 +0000

    add type revisions on leetcode #20 solution

--- 20_leet_validparentheses.py
+++ 20_leet_validparentheses.py
@@ -3,13 +3,19 @@ from typing import List
 
 class Solution:
     def isValid(self, s: str) -> bool:
-        bracket_pairs: dict[str, str] = {'(': ')', '[': ']', '{': '}'}  # set bracket pairs symbols 
+        bracket_pairs: dict[str, str] = {
+            "(": ")",
+            "[": "]",
+            "{": "}",
+        }  # set bracket pairs symbols
         stack: List[str] = []  # set a stack to store every found open bracket
         for bracket in s:
             if bracket in bracket_pairs:
                 stack.append(bracket)  # stack each open bracket found till a closed one 
                 continue
-            if not stack or bracket != bracket_pairs[stack.pop()]:  # no open bracket or no valid pair is found  
+            if (
+                not stack or bracket != bracket_pairs[stack.pop()]
+           ):  # no open bracket or no valid pair is found
                 return False
         # valid pairs were found leading to empty the stack
-        return len(stack) == 0 
+        return len(stack) == 0
