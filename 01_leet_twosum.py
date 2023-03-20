60c2d7a336d3e04ecff91b772dfc7bbd69eeff57
0xlmak <ludovic.makong@gmail.com>
2023-03-20 14:55:23 +0000

    add style revisions on optimized leetcode #01 solution

--- 01_leet_twosum.py
+++ 01_leet_twosum.py
@@ -1,7 +1,7 @@
 from typing import List, Optional
 
 
-#class Solution:
+# class Solution:
 #    def twoSum(self, nums: List[int], target: int) -> Optional[List[int]]:
 #        for i in range(len(nums)):
 #            for j in range(i + 1, len(nums)):
