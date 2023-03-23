a62d3b605ca2d76c325e6014552feeded3e82e54
0xlmak <ludovic.makong@gmail.com>
2023-03-23 12:42:28 +0000

    add optimized leetcode #189 solution

--- 189_leet_rotatearray.py
+++ 189_leet_rotatearray.py
@@ -1,6 +1,12 @@
 from typing import List
 
 
+def reverse(nums: List[int], start: int, end: int) -> None:
+        while start < end:
+            nums[start], nums[end] = nums[end], nums[start]
+            start += 1
+            end -= 1
+
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
         """
@@ -10,17 +16,7 @@ class Solution:
         nums.reverse()
         k = k % len(nums)
         # reverse the first k elements
-        start = 0
-        end = k - 1
-        while start < end:
-            nums[start], nums[end] = nums[end], nums[start]
-            start += 1
-            end -= 1
+        reverse(nums, 0, k - 1)
         # reverse the last n-k elements if nums has n elements
-        start = k
-        end = len(nums) - 1
-        while start < end:
-            nums[start], nums[end] = nums[end], nums[start]
-            start += 1
-            end -= 1
+        reverse(nums, k, len(nums) - 1)
         return None
